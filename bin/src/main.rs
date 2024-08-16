734e50a3d60fb4b8c9fcf66e3fd30c80349ce9a2
J1nH4ng <jinhang@mail.14bytes.com>
2024-08-16 11:13:18 +0800

    :sparkles: feat(main.rs && lib.rs): 可变变量和不可变变量学习

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -2,5 +2,8 @@ use variables;
 
 fn main() {
     let umut_variables_x: i32 = 10;
-    println!("{umut_variables_x} + 1 = {}", variables::umut_variables(umut_variables_x));
+    println!("{umut_variables_x} is umut and value + 1 = {}", variables::umut_variables(umut_variables_x));
+
+    let mut mut_variables_y: i32 = 10;
+    println!("{mut_variables_y} is mutable and value = {}", variables::mut_variables(&mut mut_variables_y));
 }
--- variables/src/lib.rs
+++ variables/src/lib.rs
@@ -3,12 +3,23 @@ pub fn umut_variables(x: i32) -> i32 {
     x + 1
 }
 
+pub fn mut_variables(x: &mut i32) -> i32 {
+    *x = *x + 6;
+    *x
+}
+
 #[cfg(test)]
 mod tests {
     use crate::umut_variables;
+    use crate::mut_variables;
 
     #[test]
     fn umut_variables_is_works() {
         assert_eq!(4, umut_variables(3));
+    }
+
+    #[test]
+    fn mut_variables_is_works() {
+        assert_eq!(11, mut_variables(&mut 5));
     }
 }
